ef7f39e6774506cf20a1bf3ab432673d5e9f5b0c
agentronin316 <agentronin316@users.noreply.github.com>
2022-05-18 16:27:41 +0000

    backup

---
+++ Lab_26/challenge.py
@@ -0,0 +1,3 @@
+name= input("What is your name? ")
+day_of_week= input("What is your ques... AHEM! What day of the week is it? ")
+print("Hello, " + name + "! Happy " + day_of_week +"!")
---
+++ superhero_dictionary.py
@@ -0,0 +1,8 @@
+doreen_green= {"name": "Squirrel Girl", "civilian-identity" : "Doreen Green", "debut": "November, 1991", "creators" : ["Will Murray", "Steve Ditko"], "villains-defeated": ["Doctor Doom", "Abomination", "Maelstrom", "M.O.D.O.K.", "Thanos", "Terrax", "Bi-Beast", "Deadpool", "Fin Fang Foom", "Kraven the Hunter", "Galactus", "Brain Drain", "Enigmo", "Ultron", ]}
+
+print(doreen_green)
+
+ProfessorX={'name':'Professor Charles Xavier','aliases':['Professor X','Onslaught','Consort-Royal','Founder','X','Warlord','Entity'],'base of operations':'Salem Center, New York','creator':['Stan Lee','Jack Kirby'],'bio':'Professor X (Professor Charles Xavier) is the founder of the superhero team The X-Men in the Marvel Universe and an incredibly powerful mutant hero.'}
+# name, aliases[7], base of operations, creator[2], bio
+
+print(f"Full Name: {ProfessorX['name']}\nAlias: {ProfessorX['aliases'][4]}\nBase of Operations: {ProfessorX['base of operations']}\nBiography: {ProfessorX['bio']}")
